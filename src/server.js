3ce820faf90102cb0febf31ed8a79407127c6bf7
PhilipeF <philipefdev@gmail.com>
2025-02-15 13:37:05 -0300

    Rota get e post ok

--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Testando funcionamento da api');                      
-})
+let users = [];
+
+app.get("/", (req, res) => {
+  res.status(200).json(users)
+});
+
+app.post("/user", (req, res) => {
+    users.push(req.body);
+
+    res.status(201).json(req.body);    
+});
 
 app.listen(3000, () => {
   console.log("Server is running port 3000 🚀");
 });
-
