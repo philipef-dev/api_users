7c492d3f60453fa5077bfb8ad028a0c31ff07b50
PhilipeF <philipefdev@gmail.com>
2025-02-20 17:06:40 -0300

    ate aqui tudo ok

--- src/server.js
+++ src/server.js
@@ -1,18 +1,25 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
+const prisma = new PrismaClient()
 app.use(express.json());
 
 let users = [];
 
-app.get("/", (req, res) => {
-  res.status(200).json(users)
+app.get("/users", async (req, res) => {
+  const allUsers = await prisma.user.findMany()
+  res.status(200).json(allUsers);
 });
 
 app.post("/user", (req, res) => {
-    users.push(req.body);
+  users.push(req.body);
 
-    res.status(201).json(req.body);    
+  res.status(201).json(req.body);
+});
+
+app.delete("/user/:id", (req, res) => {
+  res.send("Usuário deletado");
 });
 
 app.listen(3000, () => {
