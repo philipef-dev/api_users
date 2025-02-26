7bba5a10dadaabcc8d63ae968f6b10ef64d56d39
PhilipeF <philipefdev@gmail.com>
2025-02-26 11:58:29 -0300

    Testando

--- src/server.js
+++ src/server.js
@@ -2,24 +2,49 @@ import express from "express";
 import { PrismaClient } from "@prisma/client";
 
 const app = express();
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 app.use(express.json());
 
-let users = [];
-
 app.get("/users", async (req, res) => {
-  const allUsers = await prisma.user.findMany()
+  const allUsers = await prisma.user.findMany();
+
   res.status(200).json(allUsers);
 });
 
-app.post("/user", (req, res) => {
-  users.push(req.body);
+app.post("/user", async (req, res) => {
+  try {
+    const { name, email } = req.body;
 
-  res.status(201).json(req.body);
+    if (!name || !email) {
+      return res.status(400).json({ error: "Nome e e-mail são obrigatórios" });
+    }
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+      },
+    });
+    res.status(201).json(user);
+  } catch {
+    res.status(500).json({ error: "Erro ao criar usuário" });
+  }
 });
 
-app.delete("/user/:id", (req, res) => {
-  res.send("Usuário deletado");
+app.delete("/user/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deleteUser = await prisma.user.delete({
+      where: id,
+    });
+    res
+      .status(200)
+      .json({ message: "Usuário deletado com sucesso", deleteUser });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Erro ao deletar usuário. Verifique o ID" });
+  }
 });
 
 app.listen(3000, () => {
